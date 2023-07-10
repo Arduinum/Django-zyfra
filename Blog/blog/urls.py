bfcbd966f9c15fddc71b55b2ccc21edd9c15beef
Arduinum-628 <message.chaos628@gmail.com>
2023-07-10 11:17:53 +0300

    added: blogapp on cbv

--- Blog/blog/urls.py
+++ Blog/blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from blog import views
+from blog.views import PostListView, PostDetailView, PostFormView
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new', PostFormView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', PostFormView.as_view(), name='post_edit')
 ]
--- Blog/blog/views.py
+++ Blog/blog/views.py
@@ -1,45 +1,41 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, FormView
+from django.shortcuts import get_object_or_404, redirect
 from django.utils import timezone
 from blog.forms import PostForm
 from blog.models import Post
 
 
-def post_list(request):
-    """Контроллер для рендеринга страницы списка постов"""
-    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('-published_at')
-    return render(request=request, template_name='blog/post_list.html', context={'posts':posts})
-
-def post_detail(request, pk):
-    """Контроллер для рендеринга одного поста"""
-    print(pk)
-    post = get_object_or_404(Post, pk=pk)
-    return render(request=request, template_name='blog/post_detail.html', context={'post':post})
+class PostListView(ListView):
+    """Класс контроллер для отображения списка постов"""
+    model = Post
+    template_name = 'blog/post_list.html'
     
-def post_new(request):
-    """Контроллер для формы создания поста"""
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_at = timezone.now()
-            post.save()
-            return redirect(to='post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request=request, template_name='blog/post_edit.html', context={'form':form})
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(published_at__lte=timezone.now()).order_by('-published_at')
 
-def post_edit(request, pk):
-    """Контроллер для формы редактирования поста"""
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_at = timezone.now()
-            post.save()
-            return redirect(to='post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request=request, template_name='blog/post_edit.html', context={'form':form})
+class PostDetailView(DetailView):
+    """Класс контроллер для отображения одного поста"""
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostFormView(FormView):
+    form_class = PostForm
+    template_name = 'blog/post_edit.html'
+    
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        if self.kwargs.get('pk'):
+            kwargs['instance'] = get_object_or_404(Post, pk=self.kwargs['pk'])
+        return kwargs
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.published_at = timezone.now()
+        post.save()
+        return redirect(to='post_detail', pk=post.pk)
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
